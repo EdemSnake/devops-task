4eda923284848908e1186078633c38c8de4830c4
Edem <bewareships@gmail.com>
2025-03-14 21:59:16 +0000

    fixed

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,15 +1,18 @@
 import React, { useState } from 'react';
- import cls from"./Counter.module.scss";
-
+import cls from './Counter.module.scss';
 
 const MainPage = () => {
-
     const [count, setCount] = useState(0);
     return (
         <div className={cls.counterContainer}>
-      <h1 className={cls.counterText}>Counter: {count}</h1>
-      <button className={cls.counterButton} onClick={() => setCount(count + 1)}>+</button>
-    </div>
+            <h1 className={cls.counterText}>
+                Counter:
+                {count}
+            </h1>
+            <button type="button" className={cls.counterButton} onClick={() => setCount(count + 1)}>
+                +
+            </button>
+        </div>
     );
 };
 
